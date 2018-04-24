f35a59b3d27c831f04301bcda0d9de1eb5912959
contail <qkrtkdwls2121@gmail.com>
2018-04-24 13:45:50 +0900

    A/b

---
+++ math/src/beak_1008/Main.java
@@ -0,0 +1,17 @@
+package beak_1008;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b =sc.nextInt();
+		double result = a/(double)b;
+		
+		System.out.println(result);
+
+	}
+
+}
