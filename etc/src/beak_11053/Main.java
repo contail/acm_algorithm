780b1af35206ec609eabf6b05f633d9aaf043956
contail <qkrtkdwls2121@gmail.com>
2018-07-07 22:10:58 +0900

    가장 긴 증가하는 부분 수열

---
+++ etc/src/beak_11053/Main.java
@@ -0,0 +1,36 @@
+package beak_11053;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//수열을 담을 array
+		int arr [] = new int[10000];
+		//길이를 담을 array
+		int dp [] = new int [10000];
+		
+		int a = sc.nextInt();
+		for(int i=1; i<=a; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int max = 0;
+		for(int i=1; i<=a; i++) {
+			int min = 0;
+			for(int k=0; k<i;k++) {
+				if(arr[i] > arr[k]) {
+					if(min<dp[k])
+						min = dp[k];
+				}
+			}
+			dp[i] = min+1;
+			if(max < dp[i])
+				max = dp[i];
+			
+		}
+		
+		System.out.print(max);
+		// TODO Auto-generated method stub
+
+	}
+
+}
